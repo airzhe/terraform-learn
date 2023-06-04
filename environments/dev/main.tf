a26fa0da9ba6fa8c548e7cd77ac450432512c728
魏浩哲 <weihaozhe@popmart.com>
2023-06-04 11:33:21 +0800

    把实例类型放在main文件里统一管理

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -3,16 +3,18 @@ provider "aws" {
 }
 
 module "ec2" {
-  source = "../../modules/ec2"
+  source        = "../../modules/ec2"
+  instance_type = "t2.micro"
 }
 
 module "rds" {
-  source = "../../modules/rds"
+  source         = "../../modules/rds"
+  instance_class = "db.t4g.micro"
 
-  db_name     = "${var.env_prefix}test"
-  db_username = "runner"
-  db_password = var.db_password
-  prefix      = var.env_prefix
+  db_name        = "${var.env_prefix}test"
+  db_username    = "runner"
+  db_password    = var.db_password
+  prefix         = var.env_prefix
 
   ec2_security_group_id = module.ec2.security_group_id
 }
